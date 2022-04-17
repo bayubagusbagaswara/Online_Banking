2ac32b4ccd8e8a37dd15ae7e02696db00804a614
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-04-17 12:24:57 +0700

    added entity class Recipient

---
+++ src/main/java/com/bayu/onlinebanking/entity/Recipient.java
@@ -0,0 +1,40 @@
+package com.bayu.onlinebanking.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "recipients")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Recipient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "account_number")
+    private String accountNumber;
+
+    @Column(name = "description")
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    @JsonIgnore
+    private User user;
+}
