4901dac2d6c5f103f8873f2e4a9d3fc84d47be47
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-04-17 15:54:01 +0700

    added entity class UserRole

---
+++ src/main/java/com/bayu/onlinebanking/entity/security/UserRole.java
@@ -0,0 +1,29 @@
+package com.bayu.onlinebanking.entity.security;
+
+import com.bayu.onlinebanking.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user_role")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserRole {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long userRoleId;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+}
