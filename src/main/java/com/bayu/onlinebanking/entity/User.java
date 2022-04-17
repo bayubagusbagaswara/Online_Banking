394e324c2cb3ec51cf378d51c351a4ef03437a07
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-04-17 15:53:49 +0700

    added entity class User

---
+++ src/main/java/com/bayu/onlinebanking/entity/User.java
@@ -0,0 +1,108 @@
+package com.bayu.onlinebanking.entity;
+
+import com.bayu.onlinebanking.entity.security.Authority;
+import com.bayu.onlinebanking.entity.security.UserRole;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User implements UserDetails {
+
+    // kita implement UserDetails, agar bisa mengambil data username, password dan authorities dari User yang login
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "userId", nullable = false, updatable = false)
+    private Long userId;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "enabled")
+    private boolean enabled = true;
+
+    // 1 User hanya memiliki 1 PrimaryAccount
+    @OneToOne
+    private PrimaryAccount primaryAccount;
+
+    // 1 User hanya memiliki 1 SavingsAccount
+    @OneToOne
+    private SavingsAccount savingsAccount;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JsonIgnore
+    private List<Appointment> appointmentList;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Recipient> recipientList;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JsonIgnore
+    private Set<UserRole> userRoles = new HashSet<>();
+
+    // ini untuk mengambil authority yang dimiliki oleh seorang User
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        // create set authorities
+        Set<GrantedAuthority> authorities = new HashSet<>();
+
+        // ambil role dari userRoles
+        // yang kita ambil hanya name role nya
+        // lalu tambahkan atau masukkkan ke set authorities
+        userRoles.forEach(userRole -> authorities.add(
+                new Authority(userRole.getRole().getName())));
+        return authorities;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+}
