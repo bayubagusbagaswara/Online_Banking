91610f673becbbc3c55dcbeec2b4bd9ef429e2bd
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-04-18 20:45:09 +0700

    added implement service AccountServiceImpl

---
+++ src/main/java/com/bayu/onlinebanking/service/impl/AccountServiceImpl.java
@@ -0,0 +1,52 @@
+package com.bayu.onlinebanking.service.impl;
+
+import com.bayu.onlinebanking.entity.PrimaryAccount;
+import com.bayu.onlinebanking.entity.SavingsAccount;
+import com.bayu.onlinebanking.repository.PrimaryAccountRepository;
+import com.bayu.onlinebanking.repository.SavingsAccountRepository;
+import com.bayu.onlinebanking.service.AccountService;
+import com.bayu.onlinebanking.service.TransactionService;
+import com.bayu.onlinebanking.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.security.Principal;
+
+@Service
+public class AccountServiceImpl implements AccountService {
+
+    private static int nextAccountNumber = 11223101;
+
+    private final PrimaryAccountRepository primaryAccountRepository;
+    private final SavingsAccountRepository savingsAccountRepository;
+    private final UserService userService;
+    private final TransactionService transactionService;
+
+    @Autowired
+    public AccountServiceImpl(PrimaryAccountRepository primaryAccountRepository, SavingsAccountRepository savingsAccountRepository, UserService userService, TransactionService transactionService) {
+        this.primaryAccountRepository = primaryAccountRepository;
+        this.savingsAccountRepository = savingsAccountRepository;
+        this.userService = userService;
+        this.transactionService = transactionService;
+    }
+
+    @Override
+    public PrimaryAccount createPrimaryAccount() {
+        return null;
+    }
+
+    @Override
+    public SavingsAccount createSavingsAccount() {
+        return null;
+    }
+
+    @Override
+    public void deposit(String accountType, double amount, Principal principal) {
+
+    }
+
+    @Override
+    public void withdraw(String accountType, double amount, Principal principal) {
+
+    }
+}
