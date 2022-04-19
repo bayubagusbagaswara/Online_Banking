ca3c8f6fdfcab953a120365954c32812b001a3ad
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-04-19 18:35:14 +0700

    added implement service TransactionServiceImpl

---
+++ src/main/java/com/bayu/onlinebanking/service/impl/TransactionServiceImpl.java
@@ -0,0 +1,92 @@
+package com.bayu.onlinebanking.service.impl;
+
+import com.bayu.onlinebanking.entity.*;
+import com.bayu.onlinebanking.repository.*;
+import com.bayu.onlinebanking.service.TransactionService;
+import com.bayu.onlinebanking.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.security.Principal;
+import java.util.List;
+
+@Service
+public class TransactionServiceImpl implements TransactionService {
+
+    private final UserService userService;
+    private final PrimaryTransactionRepository primaryTransactionRepository;
+    private final SavingsTransactionRepository savingsTransactionRepository;
+    private final PrimaryAccountRepository primaryAccountRepository;
+    private final SavingsAccountRepository savingsAccountRepository;
+    private final RecipientRepository recipientRepository;
+
+    @Autowired
+    public TransactionServiceImpl(UserService userService, PrimaryTransactionRepository primaryTransactionRepository, SavingsTransactionRepository savingsTransactionRepository, PrimaryAccountRepository primaryAccountRepository, SavingsAccountRepository savingsAccountRepository, RecipientRepository recipientRepository) {
+        this.userService = userService;
+        this.primaryTransactionRepository = primaryTransactionRepository;
+        this.savingsTransactionRepository = savingsTransactionRepository;
+        this.primaryAccountRepository = primaryAccountRepository;
+        this.savingsAccountRepository = savingsAccountRepository;
+        this.recipientRepository = recipientRepository;
+    }
+
+    @Override
+    public List<PrimaryTransaction> findPrimaryTransactionList(String username) {
+        return null;
+    }
+
+    @Override
+    public List<SavingsTransaction> findSavingsTransactionList(String username) {
+        return null;
+    }
+
+    @Override
+    public void savePrimaryDepositTransaction(PrimaryTransaction primaryTransaction) {
+
+    }
+
+    @Override
+    public void saveSavingsDepositTransaction(SavingsTransaction savingsTransaction) {
+
+    }
+
+    @Override
+    public void savePrimaryWithdrawTransaction(PrimaryTransaction primaryTransaction) {
+
+    }
+
+    @Override
+    public void saveSavingsWithdrawTransaction(SavingsTransaction savingsTransaction) {
+
+    }
+
+    @Override
+    public void betweenAccountsTransfer(String transferFrom, String transferTo, String amount, PrimaryAccount primaryAccount, SavingsAccount savingsAccount) throws Exception {
+
+    }
+
+    @Override
+    public List<Recipient> findRecipientList(Principal principal) {
+        return null;
+    }
+
+    @Override
+    public Recipient saveRecipient(Recipient recipient) {
+        return null;
+    }
+
+    @Override
+    public Recipient findRecipientByName(String recipientName) {
+        return null;
+    }
+
+    @Override
+    public void deleteRecipientByName(String recipientName) {
+
+    }
+
+    @Override
+    public void toSomeoneElseTransfer(Recipient recipient, String accountType, String amount, PrimaryAccount primaryAccount, SavingsAccount savingsAccount) {
+
+    }
+}
