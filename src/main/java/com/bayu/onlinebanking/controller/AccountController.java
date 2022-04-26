2b8f18f3dc45db12d9e6050ca5bf8cf314b9b275
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-04-26 11:48:29 +0700

    added controller AccountController

---
+++ src/main/java/com/bayu/onlinebanking/controller/AccountController.java
@@ -0,0 +1,23 @@
+package com.bayu.onlinebanking.controller;
+
+import com.bayu.onlinebanking.service.AccountService;
+import com.bayu.onlinebanking.service.TransactionService;
+import com.bayu.onlinebanking.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/account")
+public class AccountController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private AccountService accountService;
+
+    @Autowired
+    private TransactionService transactionService;
+
+}
