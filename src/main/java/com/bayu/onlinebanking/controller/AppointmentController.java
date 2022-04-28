be0d644e708aabb9aa64902834db43456f5e172f
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-04-28 07:33:52 +0700

    added method createAppointment in AppointmentController

--- src/main/java/com/bayu/onlinebanking/controller/AppointmentController.java
+++ src/main/java/com/bayu/onlinebanking/controller/AppointmentController.java
@@ -1,10 +1,13 @@
 package com.bayu.onlinebanking.controller;
 
+import com.bayu.onlinebanking.entity.Appointment;
 import com.bayu.onlinebanking.service.AppointmentService;
 import com.bayu.onlinebanking.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/appointment")
@@ -19,4 +22,14 @@ public class AppointmentController {
         this.appointmentService = appointmentService;
         this.userService = userService;
     }
+
+    @RequestMapping(value = "/create", method = RequestMethod.GET)
+    public String createAppointment(Model model) {
+        Appointment appointment = new Appointment();
+        model.addAttribute("appointment", appointment);
+        model.addAttribute("dateString", "");
+
+        return "appointment";
+    }
+
 }
