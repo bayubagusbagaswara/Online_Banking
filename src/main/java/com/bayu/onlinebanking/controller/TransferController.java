4e20dc2e85e89cbd9da4a2421a684c9d129af042
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-04-28 07:44:21 +0700

    added method betweenAccounts in TransferController

--- src/main/java/com/bayu/onlinebanking/controller/TransferController.java
+++ src/main/java/com/bayu/onlinebanking/controller/TransferController.java
@@ -4,7 +4,9 @@ import com.bayu.onlinebanking.service.TransactionService;
 import com.bayu.onlinebanking.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/transfer")
@@ -18,5 +20,14 @@ public class TransferController {
     public TransferController(TransactionService transactionService, UserService userService) {
         this.transactionService = transactionService;
         this.userService = userService;
+    }
+
+    @RequestMapping(value = "/betweenAccounts", method = RequestMethod.GET)
+    public String betweenAccounts(Model model) {
+        model.addAttribute("transferFrom", "");
+        model.addAttribute("transferTo", "");
+        model.addAttribute("amount", "");
+
+        return "betweenAccounts";
     }
 }
