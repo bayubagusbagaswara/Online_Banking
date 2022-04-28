ffea4ce4a86d2b37a08b9796f5c33c3febed8f95
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-04-28 08:04:13 +0700

    added method recipientEdit in TransferController

--- src/main/java/com/bayu/onlinebanking/controller/TransferController.java
+++ src/main/java/com/bayu/onlinebanking/controller/TransferController.java
@@ -12,6 +12,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.security.Principal;
 import java.util.List;
@@ -74,5 +75,16 @@ public class TransferController {
         return "redirect:/transfer/recipient";
     }
 
+    @RequestMapping(value = "/recipient/edit", method = RequestMethod.GET)
+    public String recipientEdit(@RequestParam(value = "recipientName") String recipientName, Model model, Principal principal) {
+
+        Recipient recipient = transactionService.findRecipientByName(recipientName);
+        List<Recipient> recipientList = transactionService.findRecipientList(principal);
+
+        model.addAttribute("recipientList", recipientList);
+        model.addAttribute("recipient", recipient);
+
+        return "recipient";
+    }
 
 }
