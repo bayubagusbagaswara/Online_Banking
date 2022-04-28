7e9914882da2a3ac26ac1bf1a108d28b7daac477
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-04-28 07:39:25 +0700

    added method createAppointmentPost in AppointmentController

--- src/main/java/com/bayu/onlinebanking/controller/AppointmentController.java
+++ src/main/java/com/bayu/onlinebanking/controller/AppointmentController.java
@@ -1,13 +1,20 @@
 package com.bayu.onlinebanking.controller;
 
 import com.bayu.onlinebanking.entity.Appointment;
+import com.bayu.onlinebanking.entity.User;
 import com.bayu.onlinebanking.service.AppointmentService;
 import com.bayu.onlinebanking.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.security.Principal;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Controller
 @RequestMapping("/appointment")
@@ -32,4 +39,21 @@ public class AppointmentController {
         return "appointment";
     }
 
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    public String createAppointmentPost(@ModelAttribute("appointment") Appointment appointment,
+                                        @ModelAttribute("dateString") String date,
+                                        Model model,
+                                        Principal principal) throws ParseException {
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm");
+        Date date1 = dateFormat.parse(date);
+        appointment.setDate(date1);
+
+        User user = userService.findByUsername(principal.getName());
+        appointment.setUser(user);
+
+        appointmentService.createAppointment(appointment);
+
+        return "redirect:/userFront";
+    }
 }
