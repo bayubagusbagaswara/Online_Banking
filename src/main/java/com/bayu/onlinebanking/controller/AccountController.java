9ca62ddd7580babaad323427d46cf7eb78ed8a52
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-04-28 07:25:09 +0700

    added method deposit in AccountController

--- src/main/java/com/bayu/onlinebanking/controller/AccountController.java
+++ src/main/java/com/bayu/onlinebanking/controller/AccountController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.security.Principal;
 import java.util.List;
@@ -56,5 +57,14 @@ public class AccountController {
 
         return "savingsAccount";
     }
+
+    @RequestMapping(value = "/deposit", method = RequestMethod.GET)
+    public String deposit(Model model) {
+        model.addAttribute("accountType", "");
+        model.addAttribute("amount", "");
+
+        return "deposit";
+    }
+
 
 }
