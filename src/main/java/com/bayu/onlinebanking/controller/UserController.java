0dbb6774c46b6a723551b5e9b038df3051d6dd09
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-04-28 08:14:39 +0700

    added controller class is UserController

---
+++ src/main/java/com/bayu/onlinebanking/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.bayu.onlinebanking.controller;
+
+import com.bayu.onlinebanking.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+}
