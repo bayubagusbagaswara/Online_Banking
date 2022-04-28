9764fabd17c5065de875582a732a8af986bb2dbb
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-04-28 07:46:22 +0700

    added method betweenAccountsPost in TransferController

--- src/main/java/com/bayu/onlinebanking/controller/TransferController.java
+++ src/main/java/com/bayu/onlinebanking/controller/TransferController.java
@@ -1,12 +1,18 @@
 package com.bayu.onlinebanking.controller;
 
+import com.bayu.onlinebanking.entity.PrimaryAccount;
+import com.bayu.onlinebanking.entity.SavingsAccount;
+import com.bayu.onlinebanking.entity.User;
 import com.bayu.onlinebanking.service.TransactionService;
 import com.bayu.onlinebanking.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.security.Principal;
 
 @Controller
 @RequestMapping("/transfer")
@@ -29,5 +35,19 @@ public class TransferController {
         model.addAttribute("amount", "");
 
         return "betweenAccounts";
+    }
+
+    @RequestMapping(value = "/betweenAccounts", method = RequestMethod.POST)
+    public String betweenAccountsPost(@ModelAttribute("transferFrom") String transferFrom,
+                                      @ModelAttribute("transferTo") String transferTo,
+                                      @ModelAttribute("amount") String amount,
+                                      Principal principal) throws Exception {
+
+        User user = userService.findByUsername(principal.getName());
+        PrimaryAccount primaryAccount = user.getPrimaryAccount();
+        SavingsAccount savingsAccount = user.getSavingsAccount();
+        transactionService.betweenAccountsTransfer(transferFrom, transferTo, amount, primaryAccount, savingsAccount);
+
+        return "redirect:/userFront";
     }
 }
