534accb230ddb5c853225cdc430684061b2e815f
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-04-28 08:15:38 +0700

    added method profile in UserController

--- src/main/java/com/bayu/onlinebanking/controller/UserController.java
+++ src/main/java/com/bayu/onlinebanking/controller/UserController.java
@@ -1,9 +1,14 @@
 package com.bayu.onlinebanking.controller;
 
+import com.bayu.onlinebanking.entity.User;
 import com.bayu.onlinebanking.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @Controller
 @RequestMapping("/user")
@@ -15,4 +20,13 @@ public class UserController {
     public UserController(UserService userService) {
         this.userService = userService;
     }
+
+    @GetMapping("/profile")
+    public String profile(Principal principal, Model model) {
+        User user = userService.findByUsername(principal.getName());
+        model.addAttribute("user", user);
+
+        return "profile";
+    }
+
 }
